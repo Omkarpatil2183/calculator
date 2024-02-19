3a8f6af764a0adb8a9e83cf6535b5ac257262731
Omkar Patil <op25667@gmail.com>
2024-02-19 14:08:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const display=document.getElementById('display')
+// const display = document.getElementById('#display')
+
+
+
+
+
+function appendToDisplay(input){
+    display.value += input;
+    console.log(input);
+
+}
+
+
+function clearscreen(){
+    display.value = "";
+}
+function calculate(){
+    
+}
+
+
